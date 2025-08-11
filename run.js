5416727e2ccb47152edf9b740ab5c0b487ad4262
7yd7 <110498114+7yd7@users.noreply.github.com>
2025-08-11 19:12:35 +0300

    Update run.js

--- run.js
+++ run.js
@@ -18,8 +18,8 @@ const EXISTING_FILE = "EmoteSniper.json";
 const THUMBNAIL_API =
     "https://thumbnails.roblox.com/v1/assets?assetIds={id}&size=420x420&format=Png&isCircular=false";
 
-const CONCURRENT_REQUESTS = 10;
-const BATCH_DELAY = 2000; 
+const CONCURRENT_REQUESTS = 1;
+const BATCH_DELAY = 5000; 
 
 function log(message) {
     console.log(`[${new Date().toISOString()}] ${message}`);
